726e6c261f6a6a04708964ab33b14bc2a33c49b9
John Messina <johnvm914@gmail.com>
2017-01-22 13:05:22 -0500

    formatted pics and added restful syntax and features

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,42 @@
 class ProductsController < ApplicationController
 
-  def all_products_method
+  def index
     @products = Product.all
-    render "all_products.html.erb"
+    render "index.html.erb"
   end
 
+  def show
+    @product = Product.find_by(id: params[:id])
+    render "show.html.erb"
+  end
+
+  def new
+    render "new.html.erb"
+  end
+
+  def edit
+    @product = Product.find_by(id: params[:id])
+    render "edit.html.erb"
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.assign_attributes(name: params[:name], price: params[:price], image: params[:image], description: params[:description])
+    @product.save
+    render "update.html.erb"
+  end
+
+  def create
+    @product = Product.new(name: params[:name], price: params[:price], image: params[:image], description: params[:description])
+    @product.save
+    render "create.html.erb"
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    render "destroy.html.erb"
+  end
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  get "/all_products", to: "products#all_products_method"
+  get "/products", to: "products#index"
+  get "/products/new", to: "products#new"
+  get "/products/:id", to: "products#show"
+  get "/products/:id/edit", to: "products#edit"
+  patch "/products/:id", to: "products#update"
+  post "/products", to: "products#create"
+  delete "products/:id", to: "products#destroy"
 end
